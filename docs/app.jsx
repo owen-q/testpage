4b35510ed5ae65ea56c6595fe37acf77b8299394
Owen-q <owen.q.dev@gmail.com>
2019-01-24 22:59:03 +0900

    Update app.jsx

--- docs/app.jsx
+++ docs/app.jsx
@@ -22,63 +22,12 @@ class Header extends React.Compoent {
         );
     };
 }
-
-//---------- Body
-const MyTable = () => {
-    return (
-        <div>
-            <Table responsive>
-                <thead>
-                <tr>
-                    <th>#</th>
-                    <th>Table heading</th>
-                    <th>Table heading</th>
-                    <th>Table heading</th>
-                    <th>Table heading</th>
-                    <th>Table heading</th>
-                    <th>Table heading</th>
-                </tr>
-                </thead>
-                <tbody>
-                <tr>
-                    <td>1</td>
-                    <td>Table cell</td>
-                    <td>Table cell</td>
-                    <td>Table cell</td>
-                    <td>Table cell</td>
-                    <td>Table cell</td>
-                    <td>Table cell</td>
-                </tr>
-                <tr>
-                    <td>2</td>
-                    <td>Table cell</td>
-                    <td>Table cell</td>
-                    <td>Table cell</td>
-                    <td>Table cell</td>
-                    <td>Table cell</td>
-                    <td>Table cell</td>
-                </tr>
-                <tr>
-                    <td>3</td>
-                    <td>Table cell</td>
-                    <td>Table cell</td>
-                    <td>Table cell</td>
-                    <td>Table cell</td>
-                    <td>Table cell</td>
-                    <td>Table cell</td>
-                </tr>
-                </tbody>
-            </Table>
-        </div>
-    );
-};
-
 const MyBody = () => {
     return (
         <div>
             <h4>
                 Body
-                <MyTable />
+                
             </h4>
         </div>
     );
